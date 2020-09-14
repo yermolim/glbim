6a9fb2dcc32822dfff2c73c9a70093e65757657d
jesus <jesus@heavens.com>
2020-09-14 14:44:11 +0300

    - changed source file name

--- rollup.config.js
+++ rollup.config.js
@@ -2,7 +2,7 @@ import { terser } from "rollup-plugin-terser";
 import externals from "rollup-plugin-node-externals";
 
 export default [{
-  input: "tsc/ts-basic-gltf-viewer.js",
+  input: "tsc/gltf-viewer.js",
   output: [
     { file: "dist/ts-basic-gltf-viewer.esm.js", format: "es"},
     { file: "dist/ts-basic-gltf-viewer.esm.min.js", format: "es", plugins: [terser()] },
